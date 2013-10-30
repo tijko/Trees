452a35c40112f258078df0a05e3971ca0087b69a
Timmy Konick <konick781@gmail.com>
2013-10-30 03:22:20 -0400

    delete function

--- BST.py
+++ BST.py
@@ -32,21 +32,48 @@ def insert(tree, leaf):
         parent.left = leaf
     elif parent and leaf.value >= parent.value:
         parent.right = leaf
+    else:
+        tree = leaf
     return tree              
  
-def delete(tree):
-    pass
+def delete(tree, leaf):
+    if not leaf.left:
+       tree = transplant(tree, leaf, leaf.right)
+    elif not leaf.right:
+       tree = transplant(tree, leaf, leaf.left)
+    else:
+        _branch = tree_min(leaf.right)
+        if not _branch.parent:
+            tree = transplant(tree, _branch, _branch.right)
+            _branch.right = leaf.right
+            _branch.right.parent = _branch
+        tree = transplant(tree, leaf, _branch)
+        _branch.left = leaf.left
+        _branch.left.parent = _branch
+    return tree
 
-def search(tree, value):
-    if tree.value == value:
-        print "Value <%d> is in tree!" % value
-        return
+def transplant(tree, leaf, branch):
+    _tree = tree
+    if not leaf.parent:
+        _tree = branch
+    elif leaf == leaf.parent.left:
+        leaf.parent.left = branch
+    else:
+        leaf.parent.right = branch
+    if branch:
+        branch.parent = leaf.parent
+    return tree
+
+def search(tree, leaf):
     if not tree:
-        print "Value <%d> not in tree!" % value
-    elif tree.value < value:
-        search(tree.right, value)
+        print "Value <%d> not in tree!" % leaf.value
+    elif tree.value == leaf.value:
+        print "Value <%d> is in tree!" % leaf.value
+        return
+    elif tree.value < leaf.value:
+        search(tree.right, leaf)
     else:   
-        search(tree.left, value)
+        search(tree.left, leaf)
     
 def tree_walk(tree):
     if tree:
@@ -75,41 +102,43 @@ def tree_successor(tree):
 def tree_max(tree):
     while tree.right:
         tree = tree.right
-    return tree.value
+    return tree
 
 def tree_min(tree):
     while tree.left:
         tree = tree.left
-    return tree.value
+    return tree
 
 def return_selection():
+    leaf = None
     while True:
         try:
-            option = int(raw_input('> '))
-            return option 
+            option = int(raw_input('Enter number of option > '))
+            if option == 5:
+                return option, leaf
+            elif option != 4:
+                value = int(raw_input('Enter value > '))
+                _leaf = Leaf(value)
+                leaf = deepcopy(_leaf)
+            break
         except ValueError:
             print "Enter a number"
-
+    return option, leaf
 
 
 if __name__ == '__main__':
-    options = {1:insert, 2:delete, 3:search, 4:tree_walk}
     tree = None
     while True:
         print "Select option: [1=insert, 2=delete, 3=search, 4=tree_walk, 5=exit]"
-        option = return_selection()
-        tree_action = options.get(option)
-        if tree_action:
-            if option == 1:
-                value = return_selection() 
-                _leaf = Leaf(value)
-                leaf = deepcopy(_leaf)
-                tree = tree_action(tree, leaf)
-            elif option == 3:
-                value = return_selection()
-                tree_action(tree, value)
-            else:    
-                tree_action(tree)
+        option, leaf = return_selection()
+        if option == 1:
+            tree = insert(tree, leaf)
+        elif option == 2:
+            tree = delete(tree, leaf)
+        elif option == 3:
+            search(tree, leaf)
+        elif option == 4:    
+            tree_walk(tree)
         else:
             break
 

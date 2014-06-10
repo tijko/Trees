be02bb810f29306dfc3e75443720b19232edb7fa
Tim Konick <konick781@gmail.com>
2014-06-10 10:22:10 -0400

    replace getchar with getline

--- c_bst/BST.c
+++ c_bst/BST.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include "lib/BST_actions.h"
 
 
 int main(void) {
 
-    char choice;
+    char *line;
+    int choice;
+    ssize_t input_length;
+    size_t length = 0;
     int value;
     struct leaf *tree = NULL;
 
@@ -15,54 +19,58 @@ int main(void) {
 
         printf ("\nPick any of the following choices: [1] insert - [2] delete - [3] search - [4] print tree - [5] exit\n");
         printf ("Enter choice: ");
-        choice = getchar() - '0';
-        while ((getchar()) != '\n');
-        if (choice < 1 || choice > 5) {
-            printf("Error: not a valid choice\n");
+
+        input_length = getline(&line, &length, stdin);
+        if (input_length != 2) {
+            printf("Error: invalid choice selection.\n");
         } else {
-            switch (choice) {
+            choice = line[0] - '0';
+            if (choice < 1 || choice > 5) {
+                printf("Error: invalid choice selection.\n");
+            } else {
+                switch (choice) {
 
-                case (1):
+                    case (1):
 
-                    printf ("Enter value: ");
-                    scanf ("%d", &value);
+                        printf ("Enter value: ");
+                        scanf ("%d", &value);
 
-                    if (!(tree)) {
-                        tree = create_leaf(); 
-                        tree->value = value;
-                    }
+                        if (!(tree)) {
+                            tree = create_leaf(); 
+                            tree->value = value;
+                        }
 
-                    else {
-                        insert(&tree, value);
-                    }
+                        else {
+                            insert(&tree, value);
+                        }
 
-                    break;
-                
-                case (2):
+                        break;
+                    
+                    case (2):
 
-                    printf ("Enter value: ");
-                    scanf ("%d", &value);
-                    delete(&tree, value);
+                        printf ("Enter value: ");
+                        scanf ("%d", &value);
+                        delete(&tree, value);
 
-                    break;
+                        break;
 
-                case (3):
+                    case (3):
 
-                    printf ("Enter value: ");
-                    scanf ("%d", &value);
-                    search(tree, value);
-                    break;
+                        printf ("Enter value: ");
+                        scanf ("%d", &value);
+                        search(tree, value);
+                        break;
 
-                case (4):
+                    case (4):
 
-                    dump_tree(tree); 
+                        dump_tree(tree); 
 
-                    break;
+                        break;
 
-                case (5):
-                    return 0;
-            } 
-        }                 
-        while ((getchar()) != '\n');
+                    case (5):
+                        return 0;
+                } 
+            }                 
+        }
     }
 }

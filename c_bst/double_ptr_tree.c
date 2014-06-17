500b25e701d21157d067461e5796d0a32971c077
Tim Konick <konick781@gmail.com>
2014-06-17 13:03:44 -0400

    create separate double pointer version

---
+++ c_bst/double_ptr_tree.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib/dbl_ptr.h"
+
+
+int main(void) {
+
+    char *line;
+    int choice;
+    ssize_t input_length;
+    size_t length = 0;
+    long value;
+    int errno;
+    struct leaf *tree = NULL;
+
+    printf ("\n -*- Binary Search Tree -*- \n");
+
+    while (true) {
+
+        printf("\nPick any of the following choices: [1] insert - [2] delete - [3] search - [4] print tree - [5] exit\n");
+        printf("Enter choice: ");
+
+        input_length = getline(&line, &length, stdin);
+        if (input_length != 2) {
+            printf("Error: invalid choice selection.\n");
+        } else {
+            choice = line[0] - '0';
+            if (choice < 1 || choice > 5) {
+                printf("Error: invalid choice selection.\n");
+            } else {
+                errno = 0;
+                switch (choice) {
+
+                    case (1):
+
+                        printf("Enter value: ");
+                        input_length = getline(&line, &length, stdin);
+                        value = strtol(line, NULL, 10);
+                        if (errno != 0) {
+                            printf("Error: %s\n", strerror(errno));
+                        } else { 
+                            if (!(tree)) {
+                                tree = create_leaf(); 
+                                tree->value = value;
+                            } else {
+                                insert(&tree, value);
+                            }
+                        }
+                        break;
+                    
+                    case (2):
+
+                        printf("Enter value: ");
+                        input_length = getline(&line, &length, stdin);
+                        value = strtol(line, NULL, 10);
+                        if (errno != 0) {
+                            printf("Error: %s\n", strerror(errno));
+                        } else {
+                            delete(&tree, value);
+                        }
+                        break;
+
+                    case (3):
+
+                        printf("Enter value: ");
+                        input_length = getline(&line, &length, stdin);
+                        value = strtol(line, NULL, 10);
+                        if (errno != 0) {
+                            printf("Error: %s\n", strerror(errno));
+                        } else {
+                            search(tree, value);
+                        }
+                        break;
+
+                    case (4):
+
+                        dump_tree(tree); 
+
+                        break;
+
+                    case (5):
+                        return 0;
+                } 
+            }                 
+        }
+    }
+}

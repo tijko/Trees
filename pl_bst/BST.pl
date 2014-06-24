a00e7dc119a43ef0e345c70219b64fdb49256f55
Tim Konick <konick781@gmail.com>
2014-06-23 21:48:08 -0400

    perl binary search tree

---
+++ pl_bst/BST.pl
@@ -0,0 +1,105 @@
+use strict;
+use warnings;
+
+
+package Tree;
+
+sub create_branch {
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    return $self;
+}
+
+sub bst_menu {
+    my %tree;
+    while (1) {
+        print "Select option: [1=insert, 2=delete, 3=search, 4=tree_walk, 5=exit]\n";
+        print "Enter Selection: ";
+        my $choice = <>;
+        chomp($choice);
+        if ($choice eq 1) {
+            my $value = input_value();
+            %tree = insert(%tree, $value);
+        } elsif ($choice eq 2) {
+            my $value = input_value();
+            %tree = remove(%tree, $value);
+        } elsif ($choice eq 3) {
+            my $value = input_value();
+            search(%tree, $value);
+        } elsif ($choice eq 4) {
+            dump_tree(%tree);
+        } elsif ($choice eq 5) {
+            return;
+        } else {
+            print "Invalid Selection!\n";
+        }
+    }
+    return;
+}
+
+sub insert {
+    my ($value, %tree) = @_;
+    my %parent;
+    my %branch = %tree;
+    while (1) {
+        if (!$branch{value}) {
+            %branch = create_branch Tree();
+            $branch{value} = $value;
+            $branch{parent} = %parent;
+            last;
+        } elsif ($branch{value} ge $value) {
+            %parent = %branch;
+            %branch = $branch{left};
+        } else {
+            %parent = %branch;
+            %branch = $branch{right};
+        }
+    }
+    return %tree;
+}
+
+sub remove {
+    my ($value, %tree) = @_;
+    return %tree;
+}
+
+sub search {
+    my ($value, %tree) = @_;
+    if (!$tree{value}) {
+        print "$value is not in Tree!\n";
+    } elsif ($tree{value} eq $value) {
+        print "Found $value!\n";
+    } elsif ($tree{value} gt $value) {
+        search($tree{left}, $value);
+    } else {
+        search($tree{right}, $value);
+    }
+}
+
+sub dump_tree {
+    my %tree = @_;
+    if ($tree{value}) {
+        print "$tree{value}\n";
+        dump_tree($tree{left});
+        dump_tree($tree{right});
+    }
+    return;
+}
+
+sub input_value {
+    my $value;
+    while (1) {
+        print "Enter Value: ";
+        $value = <>;
+        chomp($value);
+        if ($value =~ /\D/) {
+            print "Invalid Value only digits...\n";
+        } else {
+            last;
+        }
+    }
+    return $value;
+}
+
+bst_menu();

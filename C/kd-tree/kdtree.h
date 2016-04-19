972f82f8f559d5dbc9c260ae7abc0c47beb83457
Tim Konick <konick781@gmail.com>
2016-04-18 22:46:38 -0400

    Prototypes for multiple types

--- C/kd-tree/kdtree.h
+++ C/kd-tree/kdtree.h
@@ -1,34 +1,35 @@
 // KD-Tree declarations
 
-#include <limits.h>
-
-
-#define MAX 65536
-#define MAX_DIST LONG_MAX
 
 struct Node {
+    int dimension;
+    double max_x;
+    double max_y;
+    double min_x;
+    double min_y;
+    void *point;
     struct Node *left;
     struct Node *right;
     struct Node *parent;
-    int *point;
-    int dimension;
-    int max_x;
-    int max_y;
-    int min_x;
-    int min_y;
 };
 
 struct Tree {
+    double max_x;
+    double max_y;
+    void *close_point;
+    void (*insert)(struct Node *node, void *point);
+    void (*closest)(struct Tree *tree, struct Node *node, void *point);
+    double (*calculate_dist_point)(struct Node *node, void *point);
+    double (*calculate_dist_rect)(struct Node *node, void *point);
     struct Node *root;
-    float close_dist;
-    int *close_coords;
+    double close_dist;
 };
 
 struct Tree *init_tree(void);
 
-struct Node *init_node(int *point);
+struct Node *init_node(void *point);
 
-void init_root(struct Tree *tree, int *point);
+void init_root(struct Tree *tree, void *point);
 
 void free_tree(struct Tree *tree);
 
@@ -36,17 +37,56 @@ void free_nodes(struct Node *node);
 
 void free_node(struct Node *node);
 
-int **create_random_points(int number_of_points);
+int **create_random_int_points(int number_of_points);
 
-void insert_node(struct Node *node, struct Node *new_node);
+long **create_random_long_points(int number_of_points);
+
+float **create_random_float_points(int number_of_points);
+
+double **create_random_double_points(int number_of_points);
+
+void insert(struct Tree *tree, void *point);
+
+void insert_int_point(struct Node *node, void *point);
+
+void insert_long_point(struct Node *node, void *point);
+
+void insert_float_point(struct Node *node, void *point);
+
+void insert_double_point(struct Node *node, void *point);
 
 void set_dimension(struct Node *node, struct Node *new_node);
 
-void insert_point_list(struct Tree *tree, int **points, int number_of_points);
+void insert_int_point_list(struct Tree *tree, int **points, int number_of_points);
 
-void closest_neighbor(struct Tree *tree, struct Node *node, int *point);
+void insert_long_point_list(struct Tree *tree, long **points, int number_of_points);
 
-float calculate_distance_point(struct Node *node, int *point);
+void insert_float_point_list(struct Tree *tree, float **points, int number_of_points);
 
-float calculate_distance_rect(struct Node *node, int *point);
+void insert_double_point_list(struct Tree *tree, double **points, int number_of_points);
 
+void closest_neighbor(struct Tree *tree, struct Node *node, void *point);
+
+void closest_int_neighbor(struct Tree *tree, struct Node *node, void *point);
+
+void closest_long_neighbor(struct Tree *tree, struct Node *node, void *point);
+
+void closest_float_neighbor(struct Tree *tree, struct Node *node, void *point);
+
+void closest_double_neighbor(struct Tree *tree, struct Node *node, void *point);
+
+double calculate_distance_int_point(struct Node *node, void *point);
+
+double calculate_distance_int_rect(struct Node *node, void *point);
+
+double calculate_distance_long_point(struct Node *node, void *point);
+
+double calculate_distance_long_rect(struct Node *node, void *point);
+
+double calculate_distance_float_point(struct Node *node, void *point);
+
+double calculate_distance_float_rect(struct Node *node, void *point);
+
+double calculate_distance_double_point(struct Node *node, void *point);
+
+double calculate_distance_double_rect(struct Node *node, void *point);

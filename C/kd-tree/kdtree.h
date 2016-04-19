444d01c3d728fddc8f71f99d955431e1dd1378ab
Tim Konick <konick781@gmail.com>
2016-04-19 17:33:12 -0400

    Updated function declarations

--- C/kd-tree/kdtree.h
+++ C/kd-tree/kdtree.h
@@ -1,5 +1,7 @@
 // KD-Tree declarations
 
+#define DIMENSION 1
+
 
 struct Node {
     int dimension;
@@ -17,10 +19,11 @@ struct Tree {
     double max_x;
     double max_y;
     void *close_point;
-    void (*insert)(struct Node *node, void *point);
+    void (*insert)(struct Tree *tree, struct Node *node, void *point);
     void (*closest)(struct Tree *tree, struct Node *node, void *point);
     double (*calculate_dist_point)(struct Node *node, void *point);
     double (*calculate_dist_rect)(struct Node *node, void *point);
+    void (*set_dimension)(struct Node *node, struct Node *new_node);
     struct Node *root;
     double close_dist;
 };
@@ -47,15 +50,21 @@ double **create_random_double_points(int number_of_points);
 
 void insert(struct Tree *tree, void *point);
 
-void insert_int_point(struct Node *node, void *point);
+void insert_int_point(struct Tree *tree, struct Node *node, void *point);
 
-void insert_long_point(struct Node *node, void *point);
+void insert_long_point(struct Tree *tree, struct Node *node, void *point);
 
-void insert_float_point(struct Node *node, void *point);
+void insert_float_point(struct Tree *tree, struct Node *node, void *point);
 
-void insert_double_point(struct Node *node, void *point);
+void insert_double_point(struct Tree *tree, struct Node *node, void *point);
 
-void set_dimension(struct Node *node, struct Node *new_node);
+void set_int_dimension(struct Node *node, struct Node *new_node);
+
+void set_long_dimension(struct Node *node, struct Node *new_node);
+
+void set_float_dimension(struct Node *node, struct Node *new_node);
+
+void set_double_dimension(struct Node *node, struct Node *new_node);
 
 void insert_int_point_list(struct Tree *tree, int **points, int number_of_points);
 
